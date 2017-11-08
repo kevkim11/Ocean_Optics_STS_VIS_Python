17ba4d85abdbedc08b3d7d30f4460e7e9b8cd222
pauline <pao-lin@comcast.net>
2017-11-07 20:39:42 -0800

    works with STS-VIS

--- main.py
+++ main.py
@@ -5,90 +5,69 @@ import seabreeze.spectrometers as sb
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import csv
 import time
 
 from drawnow import drawnow
-
-from IPython import display
-import pylab as pl
-
-# fig = plt.figure()
-# plot = fig.add_subplot(111)
 
 fig1, ax, = plt.subplots()
 plt.ion()
 
 ar_of_in = []
 
-def my_plotter(ax, y):
-    ln1 = ax.plot(y, '-b')
-    ax.xlabel('bin')
-    ax.ylabel('intensities/counts')
-    ax.legend()
-    return ln1
+# def my_plotter(ax, y):
+#     ln1 = ax.plot(y, '-b')
+#     ax.xlabel('bin')
+#     ax.ylabel('intensities/counts')
+#     ax.legend()
+#     return ln1
 
 def makeFig():
     plt.plot(ar_of_in, 'r-')
 
-def plot_intensity(ax, colors=['b']):
-    x = np.linespace(0, 1, 100)
-    if ax.lines:
-        for line in ax.lines:
-            line.set_xdata(x)
-            y = np.random.random(size=(100,1))
-            line.set_ydata(y)
-    else:
-        for color in colors:
-            y = np.random.random(size=(100,1))
-            ax.plot(x,y,color)
+# def plot_intensity(ax, colors=['b']):
+#     x = np.linespace(0, 1, 100)
+#     if ax.lines:
+#         for line in ax.lines:
+#             line.set_xdata(x)
+#             y = np.random.random(size=(100,1))
+#             line.set_ydata(y)
+#     else:
+#         for color in colors:
+#             y = np.random.random(size=(100,1))
+#             ax.plot(x,y,color)
 
-
+# 1) Get a list of devices
 devices = sb.list_devices()
 
+# 2) Get the spectrometer from the list of devices
 spec = sb.Spectrometer(device=devices[0])
-spec.integration_time_micros(integration_time_micros=10000)
+# 3) Set the integration (normally 10000 microseconds, ideally 250 microseconds)
+spec.integration_time_micros(integration_time_micros=25000)
 
 print devices
 print spec
 
 ar_of_wavelength = spec.wavelengths()
+print "!!!!!!!!!!!!!!!!!!!!!!11"
+print spec.spectrum()
 
-# while(1):
-#     ar_of_intensities = spec.intensities()
-#     print ar_of_intensities
-#     ax.plot(ar_of_wavelength, c="blue", label='intensities')
-#     plt.pause(1)
-#
+###### Just to plot the data
 # while (1):
-#     ar_of_in = spec.intensities()
+#     ar_of_in = spec.intensities(correct_nonlinearity=True)
 #     # a = np.array(ar_of_intensities)
-#     print ar_of_in
+#     # print ar_of_in
 #     drawnow(makeFig)
-#     plt.pause(1.0)
+#     plt.pause(1)
 
-# write to csv file
+###### write to csv file and plot data.
 with open('broken_pixel.csv', 'wb') as f:
     writer = csv.writer(f)
     while (1):
         ar_of_in = spec.intensities()
         # a = np.array(ar_of_intensities)
-        print ar_of_in
-        writer.writerow(ar_of_in)
+        # print ar_of_in
+        # writer.writerow(ar_of_in)
         drawnow(makeFig)
-        plt.pause(1.0)
-
-
-    # print ar_of_wavelength
-    #
-    # #Plot the wavelength and intensities from the spectrometer
-    # fig2 = plt.figure()
-    # plot2 = fig2.add_subplot(111)
-    #
-    #
-    #
-    # plot2.plot(x_axis_wavelength, ar_of_wavelength, c="red", label='wavelength')
-    #
-    # plt.show()
+        # plt.pause(1.0)
 
